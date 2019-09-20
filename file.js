33eb06c3728bc7f0bc22281b0091a8ddbeb7353e
Stian-Rojas <stianrojas15@outlook.com>
2019-09-20 13:16:24 +0200

    Add files via upload

---
+++ file.js
@@ -0,0 +1,7 @@
+var emil = prompt("er du over 18 år??");
+
+if (emil === "ja" || emil === "Ja") {
+  document.write("<p1>Emil er ikke ledig</p1>")
+} else {
+  document.write("<p1>Brur jett dette er ikke for barn</p>")
+}
